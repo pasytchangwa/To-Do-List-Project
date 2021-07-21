9671fa81375b53db2608cd1c88d6b257b10ae570
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-07-21 09:03:23 +0300

    Correct linters errors

--- src/index.js
+++ src/index.js
@@ -1,37 +1,30 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 
-const refresh = document.querySelector('.refresh')
-const form = document.querySelector('.form');
-const item = document.querySelector('#item');
-const addButton = document.querySelector('.enter');
-const clearButton = document.querySelector('#clear-btn');
-
 let lists = [];
-let index = 0;
 
 lists = [
- {
-  description: 'Wash dishes',
-  completed: false,
-  index: 0
- },
- {
-  description: 'cook',
-  completed: false,
-  index: 1
- },
- {
-  description: 'study',
-  completed: false,
-  index: 2
- },
- {
-  description: 'sleep',
-  completed: false,
-  index: 3
- }
-]
+  {
+    description: 'Wash dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'cook',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'study',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'sleep',
+    completed: false,
+    index: 3,
+  },
+];
 
 function displayel(lists) {
   const element = document.querySelector('.list-items');
@@ -41,11 +34,10 @@ function displayel(lists) {
   <p class='info'><span class='material-icons' id ='${list.index}'>
   check_box_outline_blank</span>${list.description}</p>
   <span class='material-icons delete' id ='${list.index}'>
-  more_vert</span></li>`
+  more_vert</span></li>`,
     )
     .join('');
   element.innerHTML = todoLists;
-};
+}
 
 document.addEventListener('load', displayel(lists));
-
