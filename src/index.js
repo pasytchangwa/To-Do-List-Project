2e5c6bb580b93f05a149861a590a7d3beab336a9
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-07-21 08:48:22 +0300

    Add styling and finish javaScript section

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const refresh = document.querySelector('.refresh')
+const form = document.querySelector('.form');
+const item = document.querySelector('#item');
+const addButton = document.querySelector('.enter');
+const clearButton = document.querySelector('#clear-btn');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+let lists = [];
+let index = 0;
 
-  return element;
-}
+lists = [
+ {
+  description: 'Wash dishes',
+  completed: false,
+  index: 0
+ },
+ {
+  description: 'cook',
+  completed: false,
+  index: 1
+ },
+ {
+  description: 'study',
+  completed: false,
+  index: 2
+ },
+ {
+  description: 'sleep',
+  completed: false,
+  index: 3
+ }
+]
 
-document.body.appendChild(component());
+function displayel(lists) {
+  const element = document.querySelector('.list-items');
+  const todoLists = lists
+    .map(
+      (list) => `<li class='list-item'>
+  <p class='info'><span class='material-icons' id ='${list.index}'>
+  check_box_outline_blank</span>${list.description}</p>
+  <span class='material-icons delete' id ='${list.index}'>
+  more_vert</span></li>`
+    )
+    .join('');
+  element.innerHTML = todoLists;
+};
+
+document.addEventListener('load', displayel(lists));
+
